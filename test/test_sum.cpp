0efb656bb62f3b2c1602de8b79664326be2f80e0
A00837418 <a00837418@tec.mx>
2025-06-03 23:25:20 -0600

    1

--- test/test_sum.cpp
+++ test/test_sum.cpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 TEST(EdgeTest, SortEdgesByWeight) {
-    vector<Edge> edges = {
+    std::vector<tec_ds::Utilidades::Edge> edges = {
         {0, 1, 20},
         {1, 2, 10},
         {2, 3, 30},
         {3, 4, 5}
     };
 
-    sort(edges.begin(), edges.end());
+    std::sort(edges.begin(), edges.end());
 
     EXPECT_EQ(edges[0].weight, 5);
     EXPECT_EQ(edges[1].weight, 10);
