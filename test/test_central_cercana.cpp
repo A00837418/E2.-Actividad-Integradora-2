882d11859317bbb40b86e0f0f2c4bef89b1ecafc
A00837418 <a00837418@tec.mx>
2025-06-08 23:01:19 -0600

    1

--- test/test_central_cercana.cpp
+++ test/test_central_cercana.cpp
@@ -1,42 +1,41 @@
+#include "central_cercana.h"
 #include <gtest/gtest.h>
-#include <vector>
 #include <sstream>
 #include <iostream>
-#include "tsp.h"  // incluye tu header original sin modificaciones
 
-// Clase auxiliar para capturar la salida estándar (std::cout)
+// Helper para capturar la salida estándar (cout)
 class CoutRedirect {
+    std::streambuf* old_buf;
+    std::stringstream ss;
 public:
-    CoutRedirect(std::streambuf* new_buffer) : old(std::cout.rdbuf(new_buffer)) {}
-    ~CoutRedirect() { std::cout.rdbuf(old); }
-private:
-    std::streambuf* old;
+    CoutRedirect() {
+        old_buf = std::cout.rdbuf(ss.rdbuf());
+    }
+    ~CoutRedirect() {
+        std::cout.rdbuf(old_buf);
+    }
+    std::string getString() const {
+        return ss.str();
+    }
 };
 
-TEST(TSPTest, SmallGraphTest) {
-    std::vector<std::vector<int>> adj_matrix = {
-        {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
-    };
+TEST(CentralCercanaOutputTest, ImprimeCentralYCercaniaCorrectamente) {
+    vector<pair<int,int>> centrales = { {0,0}, {10,10}, {5,5} };
+    pair<int,int> casa = {3,3};
 
-    std::stringstream buffer;
-    CoutRedirect redirect(buffer.rdbuf());  // redirige cout a buffer
+    CoutRedirect redirect;  // redirige cout a stringstream
 
-    solve_tsp(adj_matrix);
+    central_mas_cercana(centrales, casa);
 
-    std::string output = buffer.str();
+    std::string salida = redirect.getString();
 
-    // Verifica que se imprimen todas las ciudades y el costo mínimo esperado
-    EXPECT_NE(output.find("A"), std::string::npos);
-    EXPECT_NE(output.find("B"), std::string::npos);
-    EXPECT_NE(output.find("C"), std::string::npos);
-    EXPECT_NE(output.find("D"), std::string::npos);
-    EXPECT_NE(output.find("costo minimo: 80"), std::string::npos);
+    // Validamos que contenga la central correcta y la distancia aproximada
+    EXPECT_NE(salida.find("(0, 0)"), std::string::npos);
+    EXPECT_NE(salida.find("distancia:"), std::string::npos);
+
+    // Ojo: validar números exactos es complicado, pero se puede buscar "distancia: 4.242"
+    EXPECT_NE(salida.find("4.242"), std::string::npos);
 }
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+TEST(CentralCercanaOutputTest, ConUnaCentral) {
+    vector<pair<int,int>> centrales = { {2,2}
