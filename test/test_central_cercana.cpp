e3f113c3035321ab28750aebe757800a24b30261
A00837418 <a00837418@tec.mx>
2025-06-08 22:57:51 -0600

    1

--- test/test_central_cercana.cpp
+++ test/test_central_cercana.cpp
@@ -1,40 +1,42 @@
-#include "central_cercana.h"
 #include <gtest/gtest.h>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include "tsp.h"  // incluye tu header original sin modificaciones
 
-TEST(CentralCercanaTest, CasoBasico) {
-    vector<pair<int, int>> centrales = { {0,0}, {10,10}, {5,5} };
-    pair<int, int> casa = {3,3};
-    
-    auto resultado = central_mas_cercana(centrales, casa);
-    EXPECT_EQ(resultado.first, make_pair(0,0));
-    EXPECT_NEAR(resultado.second, 4.2426, 0.0001);  // sqrt(3² + 3²)
+// Clase auxiliar para capturar la salida estándar (std::cout)
+class CoutRedirect {
+public:
+    CoutRedirect(std::streambuf* new_buffer) : old(std::cout.rdbuf(new_buffer)) {}
+    ~CoutRedirect() { std::cout.rdbuf(old); }
+private:
+    std::streambuf* old;
+};
+
+TEST(TSPTest, SmallGraphTest) {
+    std::vector<std::vector<int>> adj_matrix = {
+        {0, 10, 15, 20},
+        {10, 0, 35, 25},
+        {15, 35, 0, 30},
+        {20, 25, 30, 0}
+    };
+
+    std::stringstream buffer;
+    CoutRedirect redirect(buffer.rdbuf());  // redirige cout a buffer
+
+    solve_tsp(adj_matrix);
+
+    std::string output = buffer.str();
+
+    // Verifica que se imprimen todas las ciudades y el costo mínimo esperado
+    EXPECT_NE(output.find("A"), std::string::npos);
+    EXPECT_NE(output.find("B"), std::string::npos);
+    EXPECT_NE(output.find("C"), std::string::npos);
+    EXPECT_NE(output.find("D"), std::string::npos);
+    EXPECT_NE(output.find("costo minimo: 80"), std::string::npos);
 }
 
-TEST(CentralCercanaTest, DistanciaCero) {
-    vector<pair<int, int>> centrales = { {1,1}, {2,2} };
-    pair<int, int> casa = {1,1};
-    
-    auto resultado = central_mas_cercana(centrales, casa);
-    EXPECT_EQ(resultado.first, make_pair(1,1));
-    EXPECT_DOUBLE_EQ(resultado.second, 0.0);
-}
-
-TEST(CentralCercanaTest, UnicaCentral) {
-    vector<pair<int, int>> centrales = { {100,100} };
-    pair<int, int> casa = {50,50};
-    
-    auto resultado = central_mas_cercana(centrales, casa);
-    EXPECT_EQ(resultado.first, make_pair(100,100));
-}
-
-TEST(CentralCercanaTest, VectorVacio) {
-    vector<pair<int, int>> centrales = {};
-    pair<int, int> casa = {0,0};
-    
-    EXPECT_THROW(central_mas_cercana(centrales, casa), invalid_argument);
-}
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
