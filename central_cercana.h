f3807b234915a94ff2f721358698197221e67fb9
A00837418 <a00837418@tec.mx>
2025-06-08 22:12:59 -0600

    1

--- central_cercana.h
+++ central_cercana.h
@@ -1,3 +1,12 @@
+
+/*
+ * Tec de Monterrey - Data Structures and Algorithms
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #ifndef CENTRAL_CERCANA_H
 #define CENTRAL_CERCANA_H
 
@@ -8,10 +17,26 @@
 
 using namespace std;
 
+/**
+ * @brief Calcula la distancia euclidiana entre dos puntos en un plano 2D.
+ * 
+ * @param a Coordenadas del primer punto (par de enteros: x, y).
+ * @param b Coordenadas del segundo punto (par de enteros: x, y).
+ * @return double La distancia euclidiana entre los dos puntos.
+ */
 double dist(const pair<int, int>& a, const pair<int, int>& b) {
     return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
 }
 
+/**
+ * @brief Encuentra y muestra la central más cercana a una nueva casa.
+ * 
+ * Recorre una lista de coordenadas de centrales eléctricas y determina cuál está más cerca
+ * de la casa indicada. Imprime la central más cercana y la distancia a esta.
+ * 
+ * @param centrales Vector de pares de enteros que representan las coordenadas de las centrales.
+ * @param nueva_casa Par de enteros que representa las coordenadas de la nueva casa.
+ */
 void central_mas_cercana(const vector<pair<int, int>>& centrales, const pair<int, int>& nueva_casa) {
     double min_dist = numeric_limits<double>::max();
     pair<int, int> closest = {-1, -1};
